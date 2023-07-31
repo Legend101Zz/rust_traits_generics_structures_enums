10d398a655c06261611ced2d888e122f8fe4e600
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-07-31 22:31:53 +0530

    added implementations

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 struct Person {
-citizenship : String ,
-name: String,
-age : i32,
-gender: char ,
-salary : i32
+    citizenship: String,
+    name: String,
+    age: i32,
+    gender: char,
+    salary: i32,
+}
 
-} 
+impl Person {
+    fn compute_taxes(&self) -> f32 {
+        (self.salary as f32 / 3.) * 0.5
+    }
+}
 
 fn main() {
+    let person1: Person = Person {
+        name: String::from("Mrigesh Thakur"),
+        citizenship: String::from("India"),
+        age: 20,
+        gender: 'M',
+        salary: 20_000,
+    };
 
-let person1: Person = Person {
-   name : String::from("Mrigesh Thakur") ,
-   citizenship : String::from("India"),
-   age: 20,
-   gender : 'M',
-   salary : 20_000
-};
-
-println!{"{}",person1.name}
-
+    println! {"{} tax is {}",person1.name, person1.compute_taxes()}
 }
