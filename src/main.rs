9342a47cfc0156733e0f701543ac125e28590202
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-09-18 23:39:24 +0530

    rust structs and generics

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,76 @@
-struct Person {
+ struct Person {
     citizenship: String,
     name: String,
-    age: i32,
+    age: u8,
     gender: char,
     salary: i32,
 }
 
-impl Person {
-    fn compute_taxes(&self) -> f32 {
-        (self.salary as f32 / 3.) * 0.5
-    }
+struct Student {
+    name_std: String,
+    age : u8,
+    sex: char,
+    country : String,
 }
 
-fn main() {
-    let person1: Person = Person {
-        name: String::from("Mrigesh Thakur"),
-        citizenship: String::from("India"),
-        age: 20,
-        gender: 'M',
-        salary: 20_000,
-    };
+trait GeneralInfo {
+    fn info(&self)->(&str,u8,char);
+    fn country_info(&self)-> &str ;
+}
 
-    println! {"{} tax is {}",person1.name, person1.compute_taxes()}
+// impl Person {
+
+// fn new() -> Self {
+//     Person {
+//         name: String::from("Mrigesh Thakur"),
+//         citizenship: String::from("India"),
+//         age: 20,
+//         gender: 'M',
+//         salary: 20_000,
+//     }
+
+// }
+
+
+//     fn compute_taxes(&self) -> f32 {
+//         (self.salary as f32 / 3.) * 0.5
+//     }
+// }
+
+// fn main() {
+//  let person1 = Person::new();
+
+//    let x = 2;
+
+//    let x = x +1 ;  
+
+//  let y = vec![1,2];
+       
+//     println! {"{} tax is {}",person1.name, person1.compute_taxes()}
+// }
+
+impl GeneralInfo for Person {
+fn info (&self)->(&str, u8,char){
+(&self.name , self.age, self.gender )
+}
+
+fn country_info(&self)-> &str{
+    &self.citizenship
+}
+
+}
+
+
+fn main(){
+
+
+    let person1 : Person =    Person {
+                 name: String::from("Mrigesh Thakur"),
+          citizenship: String::from("India"),
+                 age: 20,
+                 gender: 'M',
+                 salary: 20_000,
+             };
+
+             println!("{:?}", person1.info())
 }
